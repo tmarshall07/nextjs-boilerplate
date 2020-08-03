e729415bb077c73acba64b3c632b4e61d9803aba
Tanner Marshall <tanner.s.marshall@gmail.com>
2020-08-03 12:53:56 -0400

    Updated all to ts files

--- helpers/breakpoints.js
+++
@@ -1,4 +0,0 @@
-// Min-width breakpoints
-export const largeDesktopMin = 'only screen and (min-width: 981px)';
-export const desktopMin = 'only screen and (min-width: 769px)';
-export const tabletMin = 'only screen and (min-width: 481px)';
---
+++ styles/breakpoints.ts
@@ -0,0 +1,9 @@
+// Min-width breakpoints
+export const largeDesktop = 1280;
+export const desktop = 980;
+export const tablet = 768;
+export const mobile = 480;
+
+export const minLargeDesktop = `@media only screen and (min-width: ${desktop}px)`;
+export const minDesktop = `@media only screen and (min-width: ${tablet}px)`;
+export const minTablet = `@media only screen and (min-width: ${mobile}px)`;
